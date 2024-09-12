30f3d82469ee727b3011692d10b06cafdda8833e
QPM <admin@QuantumPinnacleMatrix.tech>
2024-09-13 02:23:03 +0300

    Fixing the Bug

--- server/src/auth/guards/jwt-auth.guard.ts
+++ server/src/auth/guards/jwt-auth.guard.ts
@@ -15,22 +15,26 @@ export class JwtAuthGuard implements CanActivate {
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
   ) {}
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const gqlContext = context.getArgByIndex(2);
     const req: Request = gqlContext.req;
     const token = req.cookies['access-token'];
+
     if (!token) {
       throw new UnauthorizedException('No token provided');
     }
+
     try {
-      req['user'] = this.jwtService.verifyAsync(token, {
+      const payload = await this.jwtService.verifyAsync(token, {
         secret: this.configService.getOrThrow<string>('ACCESS_TOKEN_SECRET'),
       });
+
+      req['user'] = payload;
     } catch (e) {
-      throw new UnauthorizedException('Invalid token::\n', e);
+      throw new UnauthorizedException('Invalid token', e);
     }
+
     return true;
   }
 }
